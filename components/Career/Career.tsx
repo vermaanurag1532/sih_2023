a8b5cd9fad3350a4af57ac969903b1cd9658f009
vermaanurag1532 <104077846+vermaanurag1532@users.noreply.github.com>
2023-09-20 16:49:20 +0530

    first commit

--- components/Career/Career.tsx
+++ components/Career/Career.tsx
@@ -84,7 +84,7 @@ const Goals = () => {
         </center>
       </h1>
         <center>
-      <iframe className={classes.projectVideo} width="560" height="315" src="https://www.youtube.com/embed/r-q5V6LDxEY?si=gkAYaNnMsHAn3CpW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe className={classes.projectVideo} width="560" height="315" src="https://www.youtube.com/embed/r-q5V6LDxEY?si=gkAYaNnMsHAn3CpW" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
       </center>
       </div>
     </section>
