c4802559d333bf541ac2028ccc3994604b067f10
Gaurav-Verma07 <cosmosian07@gmail.com>
2023-12-23 01:35:30 +0530

    bar chart

--- components/Features/Features.tsx
+++ components/Features/Features.tsx
@@ -1,33 +1,45 @@
-import { features } from '@/constants/featuers';
-import classes from './Features.module.css';
-import Image from 'next/image';
-import clsx from 'clsx';
-
+import { features } from "@/constants/featuers";
+import classes from "./Features.module.css";
+import Image from "next/image";
+import clsx from "clsx";
+import Chart from "react-google-charts";
+import { useState } from "react";
+export const options = {
+  chart: {
+    title: "Dustbin data",
+    subtitle: "Overall data shown as below",
+  },
+};
 const Features = () => {
+  const [dustbinData, setDustbinData] = useState<{
+    d1: number;
+    d2: number;
+    d3: number;
+  }>({
+    d1: 0,
+    d2: 0,
+    d3: 0,
+  });
+
   return (
     <section className={classes.section} id="features">
       <div className={classes.main}>
-        <h1 className={clsx(classes.heading, 'gradient')}>Applications</h1>
-        {features.map((data, index) => (
-          <div className={classes.box} key={index}>
-            <div
-              className={clsx(classes.image, index % 2 !== 0 ? classes.order1 : classes.order0)}
-              data-aos={index % 2 !== 0 ? 'fade-left' : 'fade-right'}
-            >
-              <Image src={data.img} alt="feature pic" layout="fill" />
-            </div>
-            <div className={classes.content} data-aos={index % 2 === 0 ? 'fade-left' : 'fade-right'}>
-              <h3 className={classes.title}>{data.title}</h3>
-              {/* <ul> */}
-              {data.points.map((point, index) => (
-                <p className={classes.point} key={index}>
-                  {point}
-                </p>
-              ))}
-              {/* </ul> */}
-            </div>
-          </div>
-        ))}
+        <h1 className={clsx(classes.heading, "gradient")}>Applications</h1>
+        <div className={classes.chartmain}>
+          <Chart
+            className={classes.chart}
+            chartType="Bar"
+            width="600px"
+            height="400px"
+            data={[
+              ["Type", "Level"],
+              ["Biodegradable", dustbinData.d1],
+              ["NonNiodegradable", dustbinData.d2],
+              ["Undetected", dustbinData.d3],
+            ]}
+            options={options}
+          />
+        </div>
       </div>
     </section>
   );
